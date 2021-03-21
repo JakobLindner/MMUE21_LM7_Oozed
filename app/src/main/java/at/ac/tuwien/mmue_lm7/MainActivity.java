49e6d38c0dcb857c3de846c2524a3cc84c476b0e
Jakob Lindner <jakob123lindner@gmail.com>
2021-03-21 17:00:11 +0100

    Create project

---
+++ app/src/main/java/at/ac/tuwien/mmue_lm7/MainActivity.java
@@ -0,0 +1,14 @@
+package at.ac.tuwien.mmue_lm7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
