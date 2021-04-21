7ba63b168dd17f83f23b0cb7e9e51198ba7c121f
Simon Reiser <ein.simonreiser@gmail.com>
2021-04-21 14:59:47 +0200

    Prevent endless loop when mask==0

--- app/src/main/java/at/ac/tuwien/mmue_lm7/utils/BitUtils.java
+++ app/src/main/java/at/ac/tuwien/mmue_lm7/utils/BitUtils.java
@@ -8,9 +8,12 @@ public class BitUtils {
     /**
      *
      * @param mask, number to check
-     * @return the position of the rightmost set bit, 1 for the rightmost bit
+     * @return the position of the rightmost set bit, 1 for the rightmost bit, 0 if mask is 0
      */
     public static int getRightmostSetBit(int mask) {
+        if(mask==0)
+            return 0;
+
         int pos = 1;
         int testBit = 1;
         while((testBit & mask)==0) {
