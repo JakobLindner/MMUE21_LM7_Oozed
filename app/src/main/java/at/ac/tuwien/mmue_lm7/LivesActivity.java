0b4aaf2ff8f6ecffbd9f6ca2874fe2ab844c7234
Simon Reiser <ein.simonreiser@gmail.com>
2021-05-24 15:20:40 +0200

    Style activities, create lives activity

---
+++ app/src/main/java/at/ac/tuwien/mmue_lm7/LivesActivity.java
@@ -0,0 +1,19 @@
+package at.ac.tuwien.mmue_lm7;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class LivesActivity extends AppCompatActivity {
+
+}
